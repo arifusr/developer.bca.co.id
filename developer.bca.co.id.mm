894b9c0ce24a642f7dd5178a893bfd42ab217ad2
arifusr <arifusr@gmail.com>
2020-09-19 11:36:41 +0700

    Update developer.bca.co.id.mm
    overview

--- developer.bca.co.id.mm
+++ developer.bca.co.id.mm
@@ -63,6 +63,44 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Introduction" POSITION="right" ID="ID_576343746" CREATED="1600489942531" MODIFIED="1600489954415">
+<edge COLOR="#ff0000"/>
+<node TEXT="Overview of BCA Sandbox APIs:" ID="ID_1509724984" CREATED="1600489976672" MODIFIED="1600489977175">
+<node TEXT="Get Access Token for Oauth2.0" ID="ID_287087909" CREATED="1600489982075" MODIFIED="1600489982599"/>
+<node TEXT="Get your account(s) balance" ID="ID_1554812736" CREATED="1600489988765" MODIFIED="1600489989303"/>
+<node TEXT="Get your account statement" ID="ID_631154377" CREATED="1600489993463" MODIFIED="1600489993834"/>
+<node TEXT="Fund Transfer to another BCA account" ID="ID_339431221" CREATED="1600489996942" MODIFIED="1600489997316"/>
+<node TEXT="Fund Transfer to domestic Bank account" ID="ID_1973633926" CREATED="1600490000694" MODIFIED="1600490001086"/>
+<node TEXT="&#x9;Account Statement Offline" ID="ID_1602456016" CREATED="1600490005618" MODIFIED="1600490005925"/>
+<node TEXT="Inquiry Transfer Status" ID="ID_1998742225" CREATED="1600490009959" MODIFIED="1600490010339"/>
+<node TEXT="Inquiry Domestic Account" ID="ID_1681060625" CREATED="1600490014510" MODIFIED="1600490014874"/>
+<node TEXT="&#x9;Create payment for Sakuku" ID="ID_1842485516" CREATED="1600490017665" MODIFIED="1600490018037"/>
+<node TEXT="Get Status of Sakuku Transaction" ID="ID_1293214455" CREATED="1600490022035" MODIFIED="1600490022420"/>
+<node TEXT="&#x9;Foreign Exchange Rate Information" ID="ID_506542812" CREATED="1600490025730" MODIFIED="1600490026045"/>
+<node TEXT="&#x9;BCA Branch Location" ID="ID_844288485" CREATED="1600490029503" MODIFIED="1600490029815"/>
+<node TEXT="Get balance information" ID="ID_903419763" CREATED="1600490032646" MODIFIED="1600490033016"/>
+<node TEXT="&#x9;Get beneficiary account’s information" ID="ID_312172362" CREATED="1600490035978" MODIFIED="1600490036372"/>
+<node TEXT="&#x9;Get status of a transaction" ID="ID_1512268597" CREATED="1600490039389" MODIFIED="1600490039742"/>
+<node TEXT="Transfer funds directly to beneficiary account" ID="ID_1868048663" CREATED="1600490043247" MODIFIED="1600490043630"/>
+<node TEXT="&#x9;Transfer funds to beneficiary to be taken personally" ID="ID_1766333601" CREATED="1600490047012" MODIFIED="1600490047322"/>
+<node TEXT="Amend cash transfer transaction’s detail" ID="ID_733011879" CREATED="1600490066598" MODIFIED="1600490066884"/>
+<node TEXT="&#x9;Cancel cash transfer transaction" ID="ID_1187485130" CREATED="1600490071023" MODIFIED="1600490071326"/>
+<node TEXT="Get status of payment by CompanyCode and CustomerNumber or RequestID" ID="ID_1961515205" CREATED="1600490074709" MODIFIED="1600490075083"/>
+<node TEXT="Retrieve information of Account Statement of Investor Account for being debited or credited." ID="ID_928607342" CREATED="1600490079602" MODIFIED="1600490091207"/>
+<node TEXT="Service for BCA to send notification for rejection of domestic bank transfer." ID="ID_381254967" CREATED="1600490096660" MODIFIED="1600490101913"/>
+<node TEXT="&#x9;Service for BCA to send notification of disbursement." ID="ID_1327500105" CREATED="1600490113661" MODIFIED="1600490113982"/>
+<node TEXT="Service for BCA to send notification of transaction limit update." ID="ID_326014711" CREATED="1600490132049" MODIFIED="1600490132491"/>
+<node TEXT="Service for BCA to send notification of transaction limit update." ID="ID_1090810866" CREATED="1600490144252" MODIFIED="1600490144567"/>
+<node TEXT="Send your invoice data to BCA" ID="ID_345287786" CREATED="1600490149182" MODIFIED="1600490149480"/>
+<node TEXT="Cancel your invoice data which had been sent to BCA" ID="ID_241636288" CREATED="1600490153248" MODIFIED="1600490154924"/>
+<node TEXT="Add Fund collection" ID="ID_1056690022" CREATED="1600490158656" MODIFIED="1600490158977"/>
+<node TEXT="Add data fund blocking" ID="ID_1164718726" CREATED="1600490162012" MODIFIED="1600490162351"/>
+<node TEXT="Update data fund blocking" ID="ID_641718962" CREATED="1600490165252" MODIFIED="1600490169688"/>
+<node TEXT="Release/cancel data fund blocking" ID="ID_948808846" CREATED="1600490173103" MODIFIED="1600490173414"/>
+<node TEXT="Inquiry data fund blocking" ID="ID_1744937558" CREATED="1600490176862" MODIFIED="1600490177179"/>
+</node>
+<node TEXT="" ID="ID_1116086824" CREATED="1600490187967" MODIFIED="1600490187967"/>
+</node>
 </node>
 </map>
